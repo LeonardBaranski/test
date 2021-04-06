66515e1ddc53f5e0a8d585564b1e2b629ec9e7a2
maximchristmann <mc067@hdm-stuttgart.de>
2021-04-06 17:39:03 +0200

    Commit Test

--- Test2.py
+++ Test2.py
@@ -1,6 +1,9 @@
 print("Hello World2")
-print("SchwuL")
+
 x = 29813
+print(x+1)
 
 
-print("Du bist " + x + " Meter groß")
+print("Du bist ", x, " Meter groß")
+
+lululu = "Nochmal ein wunderschöner Test"
